83234e4993f6fa03d3851c59557d478d0f878fa5
chitogex <chitogex@protonmail.com>
2020-09-10 09:21:28 +0530

    RPO-XSS

---
+++ Client Side/RPO/vulnerable.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Hello world</title>
+	<link rel="alternate" type="application/atom+xml" title="example.com" href="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);
+	?>" />
+	<link href="styles/file.css" rel="stylesheet" type="text/css"/>
+
+</head>
+<body>
+<h1>Hello world</h1>
+</body>
+</html>
+ 
